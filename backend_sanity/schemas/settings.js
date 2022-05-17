c97826256aedbc7a7d0a7e4a428685214288ac5f
sandu <sandu.andrei86@gmail.com>
2022-05-17 17:33:41 +0100

    Settings Schema created @sanity

---
+++ backend_sanity/schemas/settings.js
@@ -0,0 +1,12 @@
+export default{
+    name: 'settings',
+    title:'Settings',
+    type: 'document',
+    fields: [
+        {
+            name: 'currentCity',
+            title: 'Current City',
+            type: 'string'
+        },
+    ],
+}
