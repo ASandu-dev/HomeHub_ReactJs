4a638fc9f326cddd64f49f2750a8b3c67164867e
sandu <sandu.andrei86@gmail.com>
2022-05-18 21:47:10 +0100

    weather comp created

--- frontend_react/src/components/Weather/Weather.jsx
+++ frontend_react/src/components/Weather/Weather.jsx
@@ -1,22 +1,50 @@
-import React from "react";
 
-import { useEffect, useState } from "react";
-
+import React, { useState, useEffect } from "react";
 
 import "./Weather.scss";
 
 const Weather = () => {
-  return <div className="app__Weather app__flex">
-      <p>Weather in **city**</p>
-      <h1>temp C</h1>
-      <p>description</p>      
-  </div>;
+  let city = "Bloxwich";
+
+  const API = process.env.REACT_APP_WEATHER_API;
+  const url = `http://api.weatherapi.com/v1/current.json?key=${API}&q=${city}`;
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+
+    fetchData();
+    // eslint-disable-next-line
+  }, []);
+
+  const fetchData = async () => {
+
+    const response = await fetch(url).then((res) => res.json());
+
+    setData(response);
+  };
+
+  console.log(data, "asa");
+
+  if (data.length === []) {
+    return <h2>Loading</h2>;
+  }
+  
+  return (
+    
+    <div className="app__Weather app__flex">
+      <p>Weather in {city}</p>
+      <div className="wrapper">
+        <div className="icon">
+          <img src={data.current.condition.icon} alt="icon" />
+        </div>
+        <div className="weather_data">
+          <h1>{data.current.temp_c}°C</h1>
+          <p>{data.current.condition.text}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
-
-
-const API = process.env.REACT_APP_OPENWEATHER_API
-const URL = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={{API}}'
-
-
 
 export default Weather;
